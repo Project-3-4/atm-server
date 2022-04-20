456f0da568911e02f53c3a79ecbffe40938a437c
Niko <nicovanommen.nvo@gmail.com>
2022-04-20 12:34:02 +0000

    Middleware toegevoegd en database opgezet

--- src/index.js
+++ src/index.js
@@ -2,15 +2,51 @@ const express = require("express");
 const fs = require("fs");
 
 const app = express();
+const router = express.Router();
 const PORT = 9000;
 
 
 /**
- *@brief Promise voor het handelen van de home routing
-
+ * @brief Promise voor het instellen van de router
+ *
  */
-app.get('/', (req, res) => {
+router.use((req, res, next) => {
+	console.log("[info]\t\tMiddleware ingeschakeld!");
+	next();
+});
+router.param("authKey", (req, res, next, id) => {
+	console.log("[info]\t\tRouter param aangeroepen");
+
+	if (req.params.authKey == 400) {
+		
+	} else {
+		console.log("[error]\t\tU heeft niet de juiste bevoegdheid om de API te gebruiken!");
+		res.redirect("/api/error/401");
+	}
+	next();
+});
+
+
+/**
+ *@brief Promise voor het handelen van de home routing
+ *
+ */
+router.get("/api/:authKey/balance/get", (req, res, next) => {
 	res.send('Hellow, World');
+});
+router.post("/api/:authKey/balance/post", (req, res, next) => {
+	
+});
+router.get("/api/:authKey/withdraw/get", (req, res, next) => {
+	res.send("Money, money money");
+});
+router.post("/api/:authKey/withdraw/post", (req, res, next) => {
+	res.send("Henkie henkie henkie");
+});
+router.get("/api/error/401", (req, res, next) => {
+	res.json({
+		401: "Not authorized",
+	});
 });
 
 
@@ -18,6 +54,7 @@ app.get('/', (req, res) => {
  * @brief Functie voor het runnen van de webserver
  *
  */
+app.use('/', router);
 const server = app.listen(PORT, () => {
 	console.log(`[info]\t\tServer is running on port: ${PORT}`);
 });
