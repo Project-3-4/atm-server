fb04bd6f792cd3022f40fdd364d8fa319b133eac
RFlintstone <rubenflint@hotmail.com>
2022-05-30 12:00:01 +0200

    Removed redundant code and added some content for requests

--- src/index.js
+++ src/index.js
@@ -26,7 +26,7 @@ router.use((req, res, next) => {
 router.param("authKey", (req, res, next, id) => {
     console.log("[info]\t\tRouter param aangeroepen");
 
-    if (req.params.authKey == 400) {
+    if (req.params.authKey == 123) {
         next();
     } else {
         console.log("[error]\t\tU heeft niet de juiste bevoegdheid om de API te gebruiken!");
@@ -41,27 +41,58 @@ router.param("authKey", (req, res, next, id) => {
  */
 router.get("/api/:authKey/balance/get", (req, res, next) => {
     // When auth key is provided and balance is requested
-    res.json({balance: 123.45})
+    balanceAmount = 123.45; // Connect to DB
+
+    res.json({
+        balance: balanceAmount,
+        request: 200,
+        message: "Success"
+    })
 });
 
 router.post("/api/:authKey/balance/post", (req, res, next) => {
     // When auth key is provided and balance is requested
+    balanceAmount = 123.45; // Connect to DB
+
+    res.json({
+        balance: balanceAmount,
+        request: 200,
+        message: "Success"
+    })
 });
 
 router.get("/api/:authKey/withdraw/get", (req, res, next) => {
     // When auth key is provided and user wants to withdraw money
-    res.send("Money, money money");
+    withdrawAmount = 10; // Connect to ATM
+    oldBalanceAmount = 123.45; // Connect to DB
+
+    res.json({
+        withdraw: withdrawAmount,
+        oldBalance: oldBalanceAmount,
+        newBalance: (oldBalanceAmount - withdrawAmount),
+        request: 200,
+        message: "Success"
+    })
 });
 
 router.post("/api/:authKey/withdraw/post", (req, res, next) => {
     // When auth key is provided and user wants to withdraw money
-    res.send("Henkie henkie henkie");
+    withdrawAmount = 10; // Connect to ATM
+    oldBalanceAmount = 123.45; // Connect to DB
+
+    res.json({
+        withdraw: withdrawAmount,
+        oldBalance: oldBalanceAmount,
+        newBalance: (oldBalanceAmount - withdrawAmount), // Make variable for DB
+        request: 200,
+        message: "Success"
+    })
 });
 
 router.get("/api/error/401", (req, res, next) => {
     res.json({
-        401: "Not authorized",
-        request: "failed"
+        request: 401,
+        message: "Not authorized"
     });
 });
 
@@ -71,7 +102,7 @@ router.get("/api/error/401", (req, res, next) => {
  *
  */
 app.use('/', router);
-const server = app.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log(`[info]\t\tServer is running on port: ${PORT}`);
     console.log(`[info]\t\thttp://localhost:${PORT}`)
 });
